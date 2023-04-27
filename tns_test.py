fc8ed8a3389edada1942419ff77aa475d13b0bb0
GasperH <b.gasper.hren@gmail.com>
2023-04-27 10:56:45 +0200

    sprememba

--- tns_test.py
+++ tns_test.py
@@ -12,7 +12,12 @@ root.title("Sampanec")
 my_notebook = ttk.Notebook(root)
 my_notebook.grid(row = 0, column = 0)
 my_frame1 = ttk.Frame(my_notebook)
+my_frame2 = ttk.Frame(my_notebook)
+
+my_frame3 = ttk.Frame(my_notebook)
 my_notebook.add(my_frame1, text = "Generacija kol")
+my_notebook.add(my_frame2, text = "Razpored")
+my_notebook.add(my_frame3, text = "Koncni razpored")
 my_notebook.grid(row = 0, column = 0)
 
 mapa_za_shranjevanje = "C:\\Users\\LENOVO\\Documents\\GitHub\\Tenis_sampanec"
@@ -38,9 +43,15 @@ def generacija_kol(st_kol = 3, st_igralcev = 24):
     kola = []
     poraba = []
     breaker = False
+    print(stevilo_igralcev)
+    global preveliko_st_kol
+    global ni_deljivo_s_stiri
+    preveliko_st_kol = False
+    ni_deljivo_s_stiri = False
     
     if ((st_igralcev/4).is_integer()) == False:
         tk.messagebox.showinfo(title="Warning", message="Število igralcev ni deljivo s 4.")
+        ni_deljivo_s_stiri = True
     
     else: # če število igralcev je deljivo s 4
         [poraba.append([i]) for i in range(st_igralcev)] 
@@ -66,6 +77,7 @@ def generacija_kol(st_kol = 3, st_igralcev = 24):
                 elif j == 50000: #Če je že tolikokrat probal da izgleda ne gre
                     tk.messagebox.showinfo(title="Warning", message="Število kol je preveliko za tako število igralcev.")
                     breaker = True
+                    preveliko_st_kol = True
                 
                 else:
                     pol_dol_nabora = len(nabor)//2
@@ -79,7 +91,7 @@ def generacija_kol(st_kol = 3, st_igralcev = 24):
                             d = nabor[np.random.randint(pol_dol_nabora) + pol_dol_nabora]
                             
                             if d!=a and d!=b and d!=c and (d in poraba[a]) == False and (d in poraba[b]) == False and (d in poraba[c]) == False:
-                                kola[i].append([a,b,c,d])
+                                kola[i].append([a,c,b,d])
                                 
                                 nabor = np.delete(nabor, np.argwhere(nabor==a))
                                 nabor = np.delete(nabor, np.argwhere(nabor==b))
@@ -117,52 +129,120 @@ def generacija_in_zapis_kol():
     global st_igralcev
     global kola
     global imena_igralcev_entry
-    imena_igralcev_entry = []
-    st_kol = int(stevilo_kol.get())
-    st_igralcev = int(stevilo_igralcev.get())
+    global prvic
+    global navodilo_prvi_boben
+    global navodilo_drugi_boben
+    try:
+        st_kol = int(stevilo_kol.get())
+        st_igralcev = int(stevilo_igralcev.get())
 
-    kola = generacija_kol(st_kol, st_igralcev)
+        kola = generacija_kol(st_kol, st_igralcev)
                 
-    navodilo_prvi_boben = tk.Label(my_frame1, text = "Prvi boben").grid(row = 3, column = 0)
-    navodilo_drugi_boben = tk.Label(my_frame1, text = "Drugi boben").grid(row = 3, column = 2)
-    for i in range(st_igralcev//2): #naredijo se prostori za imena za prvi boben
-        imena_igralcev_entry.append(tk.Entry(my_frame1, width = 20))
-        imena_igralcev_entry[i].grid(row = 4+2*i, column = 0)
-        presledek = tk.Label(my_frame1, text = "").grid(row = 5+2*i, column = 0)
+        if preveliko_st_kol == False and ni_deljivo_s_stiri == False:
         
-    for i in range(st_igralcev//2): #naredijo se prostori za imena za drugi boben
-        imena_igralcev_entry.append(tk.Entry(my_frame1, width = 20))
-        imena_igralcev_entry[i + st_igralcev//2].grid(row = 4+2*i, column = 2)
+            if prvic == False:
+                for i in range(len(imena_igralcev_entry)):
+                    imena_igralcev_entry[i].destroy()
+            imena_igralcev_entry = []
+            navodilo_prvi_boben = tk.Label(my_frame1, text = "Prvi boben").grid(row = 3, column = 0)
+            navodilo_drugi_boben = tk.Label(my_frame1, text = "Drugi boben").grid(row = 3, column = 2)
+            for i in range(st_igralcev//2): #naredijo se prostori za imena za prvi boben
+                imena_igralcev_entry.append(tk.Entry(my_frame1, width = 20))
+                imena_igralcev_entry[i].grid(row = 4+2*i, column = 0)
+                presledek = tk.Label(my_frame1, text = "").grid(row = 5+2*i, column = 0)
+                
+            for i in range(st_igralcev//2): #naredijo se prostori za imena za drugi boben
+                imena_igralcev_entry.append(tk.Entry(my_frame1, width = 20))
+                imena_igralcev_entry[i + st_igralcev//2].grid(row = 4+2*i, column = 2)
+            prvic = False
+    except:
+        tk.messagebox.showinfo(title="Warning", message="Vpišite pravilno število kol in igralcev")
         
-
 def shrani_imena_v_spomin():
     global imena_igralcev
+    global rezultati_entry
+    rezultati_entry = []
     imena_igralcev = []
     for i in range(st_igralcev):
+        #imena_igralcev.append(str(i) + ". igralec")
         imena_igralcev.append(imena_igralcev_entry[i].get())    
     koncni_razpored = []
     for i in range(st_kol):
+        rezultati_entry.append([])
         koncni_razpored.append([])
+        prikaz_imen_kol = ttk.Label(my_frame2, text = str(i+1) + ". kolo").grid(row = i*(st_igralcev//4+2), column = 0)
         with open("razpored.txt", "a") as f:
                 f.write("\n"+str(i+1) + ". kolo")
                 f.close()
         for j in range(st_igralcev//4):
+            rezultati_entry[i].append([])
             koncni_razpored[i].append([])
             for k in range(4):
                 koncni_razpored[i][j].append(imena_igralcev[kola[i][j][k]])
                 if k == 1:
                     koncni_razpored[i][j].append(":")
+            for g in range(2):
+                rezultati_entry[i][j].append([])
+                rezultati_entry[i][j][g] = tk.Entry(my_frame2, width = 5)
+                rezultati_entry[i][j][g].insert(0,0)
+                rezultati_entry[i][j][g].grid(row = i*(st_igralcev//4+2)+j+1, column = 2*g+5)
+            dvopicje = tk.Label(my_frame2, text = " : ").grid(row = i*(st_igralcev//4+2)+j+1, column = 6)
+            for k in range(5):        
+                prikaz_imen_igralcev = tk.Label(my_frame2, text = koncni_razpored[i][j][k]).grid(row = i*(st_igralcev//4+2)+j+1, column = k)
             
             with open("razpored.txt", "a") as f:
                 f.write("\n")
                 f.write('\t'.join(koncni_razpored[i][j]))
-                
-    
 
-gumb_za_kola = tk.Button(my_frame1, text = "Imena igralcev", command = generacija_in_zapis_kol)
+    gumb_za_pobrati_imena["state"] = "disabled"
+    
+def preberi_rezultate_in_sestej():
+    global rezultati
+    rezultati = []
+    try:
+        for i in range(st_kol):
+            rezultati.append([])
+            for j in range(st_igralcev//4):
+                rezultati[i].append([])
+                for k in range(2):
+                    rezultati[i][j].append(int(rezultati_entry[i][j][k].get()))
+                    #rezultati[i][j].append(np.random.randint(9))
+                    
+        igralci_in_st_tock = []    
+        [igralci_in_st_tock.append([imena_igralcev[i],0]) for i in range(st_igralcev)]
+        
+        for i in range(st_kol):
+            for j in range(st_igralcev//4):
+                for k in range(2):
+                    igralci_in_st_tock[kola[i][j][2*k]][1]+=rezultati[i][j][k]
+                    igralci_in_st_tock[kola[i][j][2*k+1]][1]+=rezultati[i][j][k]
+        
+        def takeSecond(elem):
+            return elem[1]
+
+        igralci_in_st_tock.sort(key= takeSecond, reverse = True)
+        izpis_prve_vrstice = ttk.Label(my_frame3, text = "Mesto").grid(row = 0, column = 0)
+        izpis_prve_vrstice = ttk.Label(my_frame3, text = "Ime").grid(row = 0, column = 2)
+        izpis_prve_vrstice = ttk.Label(my_frame3, text = "Točke").grid(row = 0, column = 4)
+        for i in range(len(igralci_in_st_tock)):
+            izpis_rezultatov = ttk.Label(my_frame3, text = str(i + 1) + ".").grid(row = i+1, column = 0)
+            izpis_rezultatov = ttk.Label(my_frame3, text = "      ").grid(row = i+1, column = 1)
+            izpis_rezultatov = ttk.Label(my_frame3, text = igralci_in_st_tock[i][0]).grid(row = i+1, column = 2)
+            izpis_rezultatov = ttk.Label(my_frame3, text = "      ").grid(row = i+1, column = 3)
+            izpis_rezultatov = ttk.Label(my_frame3, text = igralci_in_st_tock[i][1]).grid(row = i+1, column = 4)
+            
+    except:
+        tk.messagebox.showinfo(title="Warning", message="Pravilno vpišite rezultate")
+
+prvic = True
+
+gumb_za_kola = tk.Button(my_frame1, text = "Število kol in igralcev določeno", command = generacija_in_zapis_kol)
 gumb_za_kola.grid(row = 2, column = 2)
 
 gumb_za_pobrati_imena = tk.Button(my_frame1, text = "Imena igralcev vpisana", command = shrani_imena_v_spomin)
 gumb_za_pobrati_imena.grid(row = 40, column = 3)
+
+gumb_za_prebrati_rezultate = tk.Button(my_frame2, text = "Rezultati vpisani", command = preberi_rezultate_in_sestej)
+gumb_za_prebrati_rezultate.grid(row = 0, column = 10)
 
 root.mainloop()
